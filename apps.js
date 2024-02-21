8e6a271985bf627f93f6558b3b356b105cb4ba51
Anaru03 <dele22428@uvg.edu.gt>
2024-02-21 07:29:59 -0600

    The basic structure of the page is defined

---
+++ apps.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', async function () {
+    const body = document.body;
+    const header = document.querySelector('header');
+    const main = document.querySelector('main');
+    const footer = document.querySelector('footer');
+    const spaceofmessages = document.getElementById('messages-container');
+    const SendMessage = document.getElementById('button-send');
+    const ChangeColor = document.getElementById('button-toggle-theme');
+
+}
