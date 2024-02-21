114d354fc4c150bf8ed8815336bf77f4d320cf72
Anaru03 <dele22428@uvg.edu.gt>
2024-02-21 07:44:19 -0600

    A theme change is implemented

--- apps.js
+++ apps.js
@@ -7,4 +7,40 @@ document.addEventListener('DOMContentLoaded', async function () {
     const SendMessage = document.getElementById('button-send');
     const ChangeColor = document.getElementById('button-toggle-theme');
 
-}
+    const LIghtMode = {
+        body: '#F8F8F8',
+        header: '#2c024b',
+        main: '#F6F6F6',
+        footer: '#F6F6F6',
+        message: '#2c024b',
+        border: '#EDF0F1',
+        text: '#4F4F4F'
+      };
+    
+      const DArkMode = {
+        body: '#242F38',
+        header: '#52a5e0',
+        main: '#141F27',
+        footer: '#141F27',
+        message: '#52a5e0',
+        border: '#192934',
+        text: '#D5D8DA'
+      };
+
+    function applyTheme(colors) {
+        for (let key in colors) {
+            body.style.setProperty(`--${key}`, colors[key]);
+        }
+    }
+
+    function toggleTheme() {
+        const DarkM = body.classList.toggle('dark-mode');
+        const ColorMode = DarkM ? DArkMode : LIghtMode;
+        applyTheme(ColorMode);
+        ChangeColor.innerText = DarkM ? '🕶️' : '👓';
+        localStorage.setItem('darkMode', DarkM);
+    }
+
+    ChangeColor.addEventListener('click', toggleTheme);
+
+})
